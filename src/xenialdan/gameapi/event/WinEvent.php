b101c5a6c405073e2e9015d233a7a5738f16b197
Laith Youtuber <49840784+laith98@users.noreply.github.com>
2020-02-06 09:47:09 -0800

    Create WinEvent.php

---
+++ src/xenialdan/gameapi/event/WinEvent.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace xenialdan\gameapi\event;
+
+use pocketmine\event\plugin\PluginEvent;
+use pocketmine\Player;
+use pocketmine\plugin\Plugin;
+use pocketmine\Server;
+use pocketmine\utils\TextFormat;
+use xenialdan\gameapi\API;
+use xenialdan\gameapi\Arena;
+use xenialdan\gameapi\Team;
+
+class WinEvent extends PluginEvent{
+	public static $handlerList = null;
+	/** @var Arena */
+	private $arena;
+	private $winner;
+
+	/**
+	 * TeamWinEvent constructor.
+	 * @param Plugin $plugin
+	 * @param Arena $arena
+	 * @param Team|Player $winner
+	 */
+	public function __construct(Plugin $plugin, Arena $arena, $winner){
+		parent::__construct($plugin);
+		$this->arena = $arena;
+		$this->winner = $winner;
+	}
+
+	public function announce(){
+		$prefix = $this->winner instanceof Player ? "Player " : "Team ";
+        Server::getInstance()->broadcastTitle(TextFormat::GREEN . $prefix . $this->winner->getName(), TextFormat::GREEN . ' has won the game ' . $this->arena->getOwningGame()->getPrefix() . '!', -1, -1, -1, Server::getInstance()->getDefaultLevel()->getPlayers());
+        Server::getInstance()->broadcastMessage(TextFormat::GREEN . $prefix . $this->winner->getName() . TextFormat::GREEN . ' has won the game ' . $this->arena->getOwningGame()->getPrefix() . '!', Server::getInstance()->getDefaultLevel()->getPlayers());
+	}
+
+	public function getGame(){
+		return API::getGame($this->getPlugin()->getName());
+	}
+
+	/**
+	 * @return Player[]
+	 */
+	public function getWinningPlayers(){
+		if($this->winner instanceof Player) return [$this->winner];
+		else return $this->winner->getInitialPlayers();
+	}
+}
